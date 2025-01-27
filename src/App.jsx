ac9d56c55fda1dcca1bacdac135eaf0620174972
v-kills <veronica.j.killingsworth@gmail.com>
2025-01-27 11:32:41 -0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Layout from './components/Layout';
-import Header from './components/header';
+// import Header from './components/header';
 
 function App() {
  
   return (
     <BrowserRouter>
     <div className='App'>
-    <Header/>
+    {/* <Header/> */}
       <Routes>
         <Route path="/" element={<Layout />}/>
         <Route index element={<Home />} />
