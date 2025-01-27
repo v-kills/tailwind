99e8e6229b92987a5d3c74de44aa204d4dab55b1
v-kills <veronica.j.killingsworth@gmail.com>
2025-01-27 11:40:06 -0500

    why is header not working

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,9 @@
 import { Outlet } from 'react-router-dom';
-import Header from './header';
+
 function Layout() {
   return (
     <div className='root'>
-    <div className='header'>
-        <Header/>
-    </div>
+
       {/* put nav bar here */}
       <main>
         <Outlet />
